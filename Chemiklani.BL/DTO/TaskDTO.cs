7a4dab52e1b031a59ef2c2e553a5be84d76f8ba2
Jan Skála <j.skala@hotmail.com>
2017-07-07 16:55:38 +0200

    Add view and logic for tasks.

---
+++ Chemiklani.BL/DTO/TaskDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chemiklani.BL.DTO
+{
+    public class TaskDTO:BaseDTO
+    {
+        [Required(ErrorMessage = "Musíte vyplnit název úlohy.")]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public int MaximumPoints { get; set; }
+    }
+}
--- Chemiklani/ViewModels/TasksViewModel.cs
+++ Chemiklani/ViewModels/TasksViewModel.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DotVVM.Framework.ViewModel;
+using System.Threading.Tasks;
+using Chemiklani.BL.DTO;
+using Chemiklani.BL.Services;
 
 namespace Chemiklani.ViewModels
 {
@@ -10,6 +9,28 @@ namespace Chemiklani.ViewModels
 	{
 	    public override string PageTitle => "Úlohy";
 	    public override string PageDescription => "Správa úloh a jejich bodového ohodnocení.";
+
+	    public List<TaskDTO>Tasks { get; set; } = new List<TaskDTO>();
+	    public TaskDTO NewTask { get; set; } = new TaskDTO();
+
+        private readonly TaskService service = new TaskService();
+
+	    public override Task PreRender()
+	    {
+	        Tasks = service.LoadTasks();
+	        return base.PreRender();
+	    }
+
+	    public void AddTask()
+	    {
+            service.AddTask(NewTask);
+	        NewTask = new TaskDTO();
+	    }
+
+	    public void DeleteTask(int id)
+	    {
+	        service.Delete(id);
+	    }
 	}
 }
 
