9e86d66ab84f51dbaaa7c44ec8dfbec2a829f727
Jan Skála <j.skala@hotmail.com>
2017-07-24 10:05:44 +0200

    Prepare data objects.

---
+++ Chemiklani.BL/DTO/ResultEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Chemiklani.BL.DTO
+{
+    public class ResultEntry
+    {
+        public string Room { get; set; }        
+        public string TeamName { get; set; }
+        public int Ammount { get; set; }
+        public double AveragePointsForTask { get; set; }
+        public int TotalPoints { get; set; }
+        public List<TaskScoreDTO> TaskScores { get; set; }
+    }
+
+    public class ResultSummary
+    {
+        
+    }
+}
---
+++ Chemiklani.BL/Utils/Results.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Chemiklani.BL.DTO;
+
+namespace Chemiklani.BL.Utils
+{
+    public class Results
+    {
+        public string GenerateCompleteCsv(List<TeamScoreDTO> scores)
+        {
+            return "";
+        }
+    }
+}
