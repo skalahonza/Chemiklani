cfa18c99a74ebbe4f262fd5d2a0dce254fd11597
Jan Skála <j.skala@hotmail.com>
2017-07-27 10:46:09 +0200

    Results exeptions handling.

--- Chemiklani/ViewModels/MasterPageViewModel.cs
+++ Chemiklani/ViewModels/MasterPageViewModel.cs
@@ -9,79 +9,82 @@ using Microsoft.ApplicationInsights;
 namespace Chemiklani.ViewModels
 {
     [Authorize]
-	public abstract class MasterPageViewModel : DotvvmViewModelBase
-	{
+	public abstract class MasterPageViewModel : SafeViewModel
+    {
 	    public abstract string PageTitle { get; }
         public abstract string PageDescription { get; }
 	    public string AppName => "Chemiklání";
 	    public string CurrentRoute => Context.Route.RouteName;
 	    public string CurrentUserName => Context.GetAuthentication().User.Identity.Name;
+    }
 
-	    public bool IsDialogDisplayed { get; set; }
-	    [Bind(Direction.ServerToClient)]
-	    public string AlertText { get; set; }
+    public class SafeViewModel : DotvvmViewModelBase
+    {
+        public bool IsDialogDisplayed { get; set; }
+        [Bind(Direction.ServerToClient)]
+        public string AlertText { get; set; }
 
-	    [Bind(Direction.ServerToClient)]
-	    public AlertType AlertType { get; set; }
+        [Bind(Direction.ServerToClient)]
+        public AlertType AlertType { get; set; }
 
         public void SignOut()
-	    {
-	        Context.GetAuthentication().SignOut();
+        {
+            Context.GetAuthentication().SignOut();
             Context.RedirectToRoute("SignIn");
-	    }
-
-	    protected void SetSuccess(string message)
-	    {
-	        IsDialogDisplayed = true;
-	        AlertText = message;
-	        AlertType = AlertType.Success;
         }
 
-	    protected void SetError(string message)
-	    {
-	        IsDialogDisplayed = true;
-	        AlertText = message;
-	        AlertType = AlertType.Danger;
-	    }
+        protected void SetSuccess(string message)
+        {
+            IsDialogDisplayed = true;
+            AlertText = message;
+            AlertType = AlertType.Success;
+        }
 
-	    protected void SetWarning(string message)
-	    {
-	        IsDialogDisplayed = true;
-	        AlertText = message;
-	        AlertType = AlertType.Warning;
-	    }
+        protected void SetError(string message)
+        {
+            IsDialogDisplayed = true;
+            AlertText = message;
+            AlertType = AlertType.Danger;
+        }
 
-	    protected bool ExecuteSafe(Action action)
-	    {
-	        try
-	        {
-	            action.Invoke();
-	            return true;
-	        }
-	        catch (DotvvmInterruptRequestExecutionException)
-	        {
-	            throw;
-	        }
-	        
+        protected void SetWarning(string message)
+        {
+            IsDialogDisplayed = true;
+            AlertText = message;
+            AlertType = AlertType.Warning;
+        }
+
+        protected bool ExecuteSafe(Action action)
+        {
+            try
+            {
+                action.Invoke();
+                return true;
+            }
+            catch (DotvvmInterruptRequestExecutionException)
+            {
+                throw;
+            }
+
             catch (AppLogicException e)
-	        {
-	            SetError(e.Message);
-	            return false;
-	        }
+            {
+                SetError(e.Message);
+                return false;
+            }
 
-	        catch (Exception ex)
-	        {
-	            var telemetry = new TelemetryClient();
+            catch (Exception ex)
+            {
+                var telemetry = new TelemetryClient();
                 SetError("V aplikaci došlo k neznámé chybì. Chyba byla zaznamenána, ale mùžete kontaktovat správce a popsat mu jak k ní došlo.");
                 telemetry.TrackException(ex);
-	            return false;
-	        }
+                return false;
+            }
         }
 
-	    public virtual void DialogAction()
-	    {
-	        IsDialogDisplayed = false;
-	    }
+        public virtual void DialogAction()
+        {
+            IsDialogDisplayed = false;
+        }
     }
 }
 
